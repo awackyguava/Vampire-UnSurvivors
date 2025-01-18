6fe490e5c09dc8ebe8872d7126ae2fc307237de9
awackyguava <107325574+awackyguava@users.noreply.github.com>
2025-01-18 20:56:01 +0000

    added custom world i made

--- py/main.py
+++ py/main.py
@@ -2,6 +2,7 @@ from settings import *
 from player import Player
 from sprites import *
 from random import randint
+from pytmx.util_pygame import load_pygame
 
 class Game:
     def __init__(self):
@@ -18,12 +19,32 @@ class Game:
         ## Groups ##
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
+
+        self.map_setup()
         
         ## Sprites ##
         self.player = Player((400,300), player_sheet, 2, 0, self.all_sprites, self.collision_sprites)
-        for i in range(6):
-            Collision((randint(0,WINDOW_WIDTH),randint(0,WINDOW_HEIGHT)), (randint(60,100),randint(40,80)), (self.all_sprites,self.collision_sprites))
     
+    def map_setup(self):
+        map = load_pygame(join('data', 'levels', 'level1.tmx'))
+
+        ## Tile Layer ##
+        for x, y, image in map.get_layer_by_name('Ground').tiles():
+            Tile((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
+
+        ## Deco layer ##
+        for x, y, image in map.get_layer_by_name('Deco').tiles():
+            Deco((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
+
+        ## Geometry Layer ##
+        for object in map.get_layer_by_name('Geometry'):
+            if object.name == 'wall':
+                Collision((object.x, object.y), pygame.Surface((object.width, object.height)), self.collision_sprites)
+
+        ## Collidables Layer ##
+        for object in map.get_layer_by_name('Collidables'):
+            Collision((object.x, object.y), object.image, (self.all_sprites, self.collision_sprites))
+
     def exe(self):
         while self.running:
             ## dt ##
--- py/sprites.py
+++ py/sprites.py
@@ -1,8 +1,20 @@
 from settings import *
 
 class Collision(pygame.sprite.Sprite):
-    def __init__(self, pos, size, groups):
+    def __init__(self, pos, surface, groups):
         super().__init__(groups)
-        self.image = pygame.Surface(size)
-        self.image.fill('blue')
-        self.rect = self.image.get_frect(center = pos)
+        self.image = surface
+        self.rect = self.image.get_frect(topleft = pos)
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, pos, surface, groups):
+        super().__init__(groups)
+        self.image = surface
+        self.rect = self.image.get_frect(topleft = pos)
+
+class Deco(pygame.sprite.Sprite):
+    def __init__(self, pos, surface, groups):
+        super().__init__(groups)
+        self.image = surface
+        self.rect = self.image.get_frect(topleft = pos)
+
